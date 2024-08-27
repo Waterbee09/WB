ef6d92ba46f74c3b167526a78b070e6193214157
Waterbee09 <132153597+Waterbee09@users.noreply.github.com>
2024-08-27 15:50:27 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-# 6. ตั้งค่า CID (Customer ID) สำหรับ Falcon Sensor
-echo "กำลังตั้งค่า CID..."
-/opt/CrowdStrike/falconctl -s --cid=00F79DE8BE94463D8482591A1A4B5AF2-85
-sleep 10  # รอ 10 วินาที
-
-
-sleep 10  # รอ 10 วินาที
